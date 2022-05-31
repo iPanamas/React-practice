290747df5eb1c883918d7cbe5d77af23a1c3561c
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-05-31 23:05:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ import Container from './components/Module-1 Components and style/Container/Cont
 // import ColorPicker from './components/Module-2 Events, state. Forms/ColorPicker/ColorPicker';
 import ToDoList from './components/Module-2 Events, state. Forms/ToDoList/ToDoList';
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 export default function App() {
   return (
