50d1810fb387804550915b68a89e1129efc4f29a
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-06-09 01:12:49 +0300

    update

--- src/components/HTTPRequest/api.jsx
+++ src/components/HTTPRequest/api.jsx
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 const fetchArticlesWithQuery = async searchQuery => {
   const response = await axios.get(`/search?query=${searchQuery}`);
+  console.log(response.data.hits);
   return response.data.hits;
 };
 
 export default {
   fetchArticlesWithQuery,
 };
-
-// asdasdsa
